2791e3e0cbf36a3f71dbdfd74b93d017dd7ded74
scor3141o <dkosimov@gmail.com>
2020-03-11 16:24:36 +0100

    Second swing app

--- SwingApp/src/FutureValueApp.java
+++ SwingApp/src/FutureValueApp.java
@@ -12,5 +12,6 @@ class FutureValueFrame extends JFrame {
         setTitle("Future Value Calculator");
         setBounds(267, 200, 267, 200);
         setResizable(false);
+//        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
