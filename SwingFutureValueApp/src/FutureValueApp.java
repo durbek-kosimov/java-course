25507de36286a64da7fd994aa1faeba2e5c9265c
scor3141o <dkosimov@gmail.com>
2020-03-14 22:51:57 +0100

    Future value app

---
+++ SwingFutureValueApp/src/FutureValueApp.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class FutureValueApp {
+    public static void main(String[] args) {
+        JFrame frame = new FutureValueFrame();
+        frame.setVisible(true);
+    }
+}
+
+
+
