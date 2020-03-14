f3b5afb89d31dd7ce00f09f1ed7ca76c9216a7e2
scor3141o <dkosimov@gmail.com>
2020-03-14 22:53:44 +0100

    Future Value Frame

---
+++ SwingFutureValueApp/src/FutureValueFrame.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class FutureValueFrame extends JFrame {
+    public FutureValueFrame() {
+        setTitle("Future Value Calculator");
+        setSize(267, 200);
+        centerWindow(this);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel panel = new FutureValuePanel();
+        this.add(panel);
+    }
+
+    private void centerWindow(Window w) {
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        Dimension d = tk.getScreenSize();
+        setLocation((d.width - w.getWidth()) / 2, (d.height - w.getHeight() / 2));
+    }
+}
