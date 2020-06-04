8453abadbb19704b3b4009e608f9eae2da6d46dd
scor3141o <dkosimov@gmail.com>
2020-06-04 13:32:33 +0200

    String template

--- JavaSE/src/StringTemplate.java
+++ JavaSE/src/StringTemplate.java
@@ -1,9 +1,10 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionListener;
 
 public class StringTemplate extends JFrame implements ActionListener {
     private JTextField string1Field, getString2Field, resultField;
-    private JLabel string1Label, getString2Label, resultLabel;
+    private JLabel string1Label, string2Label, resultLabel;
     private JButton goButton;
 
     public static void main(String[] args) {
@@ -13,5 +14,18 @@ public class StringTemplate extends JFrame implements ActionListener {
         frame.setVisible(true);
     }
 
-    private void createGUI() {}
+    private void createGUI() {
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        Container window = getContentPane();
+        window.setLayout(new FlowLayout());
+
+        string1Label = new JLabel("Type string1: ");
+        window.add(string1Label);
+
+        string1Field = new JTextField(20);
+        window.add(string1Field);
+
+        string2Label = new JLabel("Type string2: ");
+        window.add(string2Label);
+    }
 }
