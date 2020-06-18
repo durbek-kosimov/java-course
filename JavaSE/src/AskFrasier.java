d604c9bdca856678c87a38d83b9c77ee371a58c4
scor3141o <dkosimov@gmail.com>
2020-06-18 10:38:31 +0200

    Ask Frasier

---
+++ JavaSE/src/AskFrasier.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class AskFrasier extends JFrame implements ActionListener {
+    private JTextField questionField, replyField;
+    private JLabel psychiatristLabel, questionLabel;
+    private Psychiatrist frasier;
+}
