3bc03115c8035ef49a0e18b1c013924203351e32
scor3141o <dkosimov@gmail.com>
2020-06-25 07:17:35 +0200

    ExceptionDemo1

--- JavaSE/src/ExceptionDemo1.java
+++ JavaSE/src/ExceptionDemo1.java
@@ -4,4 +4,6 @@ import java.awt.event.ActionListener;
 public class ExceptionDemo1 extends JFrame implements ActionListener {
     private JTextField inputField;
     private JTextField resultField;
+    private JLabel resultLabel, inputLabel;
+    private JButton doubleButton;
 }
