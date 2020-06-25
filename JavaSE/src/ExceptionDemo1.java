f9fb4840ae433dfb48df92bdfa29f80c1ee5396b
scor3141o <dkosimov@gmail.com>
2020-06-25 07:16:23 +0200

    ExceptionDemo1

---
+++ JavaSE/src/ExceptionDemo1.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class ExceptionDemo1 extends JFrame implements ActionListener {
+    private JTextField inputField;
+    private JTextField resultField;
+}
