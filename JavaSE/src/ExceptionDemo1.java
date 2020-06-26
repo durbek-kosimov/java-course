c444dfc8dbad0188824f49972e721b97c2f71253
scor3141o <dkosimov@gmail.com>
2020-06-26 10:13:18 +0200

    ExceptionDemo1

--- JavaSE/src/ExceptionDemo1.java
+++ JavaSE/src/ExceptionDemo1.java
@@ -6,4 +6,11 @@ public class ExceptionDemo1 extends JFrame implements ActionListener {
     private JTextField resultField;
     private JLabel resultLabel, inputLabel;
     private JButton doubleButton;
+
+    public static void main(String[] args) {
+        ExceptionDemo1 frame = new ExceptionDemo1();
+        frame.setSize(300, 150);
+        frame.creationGUI();
+        frame.setVisible(true);
+    }
 }
