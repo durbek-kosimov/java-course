1bfa8d4406bb6584dcfeeae7c2936504ac247866
scor3141o <dkosimov@gmail.com>
2020-07-02 14:45:32 +0200

    FileOutputDemo

---
+++ JavaSE/src/FileOutputDemo.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class FileOutputDemo extends JFrame implements ActionListener {
+    private JTextArea textArea;
+    private JButton saveButton;
+}
