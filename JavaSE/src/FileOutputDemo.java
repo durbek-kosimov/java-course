1263ea3e0472057e9d3ddbd82ba8e01899e7bd79
scor3141o <dkosimov@gmail.com>
2020-07-03 07:16:52 +0200

    FileOutputDemo

--- JavaSE/src/FileOutputDemo.java
+++ JavaSE/src/FileOutputDemo.java
@@ -1,7 +1,11 @@
 import javax.swing.*;
 import java.awt.event.ActionListener;
+import java.io.PrintWriter;
 
 public class FileOutputDemo extends JFrame implements ActionListener {
     private JTextArea textArea;
     private JButton saveButton;
+    private JTextField nameField;
+    private JLabel nameLabel;
+    private PrintWriter outFiles;
 }
