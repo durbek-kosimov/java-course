478773bf909c43207460ef344c7f7ee537f00bd1
scor3141o <dkosimov@gmail.com>
2020-07-07 06:03:30 +0200

    FileOutputDemo

---
+++ JavaSE/src/Hello2.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Hello2 extends JFrame {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Hello, World!");
+        JOptionPane.showMessageDialog(null,"Goodbye");
+        JOptionPane.showMessageDialog(null,"Finishing now");
+        System.exit(0);
+    }
+}
