864e222f83009c62ee86b8b4efd3dab7cda7b687
scor3141o <dkosimov@gmail.com>
2020-07-09 10:39:04 +0200

    FileInputDemo

---
+++ JavaSE/src/FileInputDemo.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class FileInputDemo extends JFrame implements ActionListener {
+    private JTextArea textArea;
+    private JButton openButton;
+}
