9e0970655acc2738421acf777c87fef771329b2c
scor3141o <dkosimov@gmail.com>
2020-07-09 10:40:31 +0200

    FileInputDemo

--- JavaSE/src/FileInputDemo.java
+++ JavaSE/src/FileInputDemo.java
@@ -1,7 +1,11 @@
 import javax.swing.*;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 
 public class FileInputDemo extends JFrame implements ActionListener {
     private JTextArea textArea;
     private JButton openButton;
+    private BufferedReader inFile;
+    private JTextField nameField;
+    private JLabel nameLabel;
 }
