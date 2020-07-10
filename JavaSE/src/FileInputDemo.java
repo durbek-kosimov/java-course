0d9f5976a840365b98a353edcdc1f638a164af04
scor3141o <dkosimov@gmail.com>
2020-07-10 15:46:27 +0200

    FileInputDemo

--- JavaSE/src/FileInputDemo.java
+++ JavaSE/src/FileInputDemo.java
@@ -8,4 +8,11 @@ public class FileInputDemo extends JFrame implements ActionListener {
     private BufferedReader inFile;
     private JTextField nameField;
     private JLabel nameLabel;
+
+    public static void main(String[] args) {
+        FileInputDemo frame = new FileInputDemo();
+        frame.setSize(400, 300);
+        frame.createGUI();
+        frame.setVisible(true);
+    }
 }
