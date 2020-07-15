4d254ab1123bbd31bb58d40b416b87b71f82e95d
scor3141o <dkosimov@gmail.com>
2020-07-15 06:10:53 +0200

    FileSearch

--- JavaSE/src/FileSearch.java
+++ JavaSE/src/FileSearch.java
@@ -6,4 +6,8 @@ import java.io.BufferedReader;
 public class FileSearch extends JFrame implements ActionListener {
     private BufferedReader inFile;
     private Button searchButton;
+    private JTextField result1Field;
+    private JTextField result2Field;
+    private JTextField personField;
+    private JTextField fileNameField;
 }
