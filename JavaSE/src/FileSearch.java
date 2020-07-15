d01f68a04562775e52ddbeef6019af4304319dc4
scor3141o <dkosimov@gmail.com>
2020-07-15 06:09:02 +0200

    FileSearch

---
+++ JavaSE/src/FileSearch.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+
+public class FileSearch extends JFrame implements ActionListener {
+    private BufferedReader inFile;
+    private Button searchButton;
+}
