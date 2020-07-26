563f5076fd0750bccdbdd94e63c2651d26c74704
scor3141o <dkosimov@gmail.com>
2020-07-26 16:13:22 +0200

    UseBallon2

--- JavaSE/src/UseBalloon2.java
+++ JavaSE/src/UseBalloon2.java
@@ -4,14 +4,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class UseBalloon2 extends JFrame implements ActionListener {
-    private JButton growButton, moveButton;
+    private JButton growButton, moveRightButton, moveLeftButton, moveUpButton, moveDownButton;
     private JPanel panel;
 
     private Balloon2 balloon;
 
     public static void main(String[] args) {
         UseBalloon2 demo = new UseBalloon2();
-        demo.setSize(250,300);
+        demo.setSize(250,350);
         demo.createGUI();
         demo.setVisible(true);
     }
@@ -26,21 +26,39 @@ public class UseBalloon2 extends JFrame implements ActionListener {
         panel.setBackground(Color.white);
         window.add(panel);
 
-        moveButton = new JButton("move");
-        window.add(moveButton);
-        moveButton.addActionListener(this);
+        moveRightButton = new JButton("move right");
+        window.add(moveRightButton);
+        moveRightButton.addActionListener(this);
+
+        moveLeftButton = new JButton("move left");
+        window.add(moveLeftButton);
+        moveLeftButton.addActionListener(this);
+
+        moveUpButton = new JButton("move up");
+        window.add(moveUpButton);
+        moveUpButton.addActionListener(this);
+
+        moveDownButton = new JButton("move down");
+        window.add(moveDownButton);
+        moveDownButton.addActionListener(this);
 
         growButton = new JButton("grow");
         window.add(growButton);
         growButton.addActionListener(this);
 
-        balloon = new Balloon2();
+        balloon = new Balloon2(50,50,20,"Balloon2");
     }
 
     public void actionPerformed(ActionEvent event) {
         Graphics paper = panel.getGraphics();
-        if (event.getSource() == moveButton)
+        if (event.getSource() == moveRightButton)
             balloon.moveRight(20);
+        else if (event.getSource() == moveLeftButton)
+            balloon.moveLeft(20);
+        else if (event.getSource() == moveUpButton)
+            balloon.moveUp(20);
+        else if (event.getSource() == moveDownButton)
+            balloon.moveDown(20);
         else
             balloon.changeSize(20);
         paper.setColor(Color.white);
@@ -50,10 +68,17 @@ public class UseBalloon2 extends JFrame implements ActionListener {
 }
 
 class Balloon2 {
-    private int x = 50;
-    private int y = 50;
-    private int diameter = 20;
-    private String name = "Ballon2";
+    private int x;
+    private int y;
+    private int diameter;
+    private String name;
+
+    public Balloon2(int x, int y, int diameter, String name) {
+        this.x = x;
+        this.y = y;
+        this.diameter = diameter;
+        this.name = name;
+    }
 
 
     public void moveRight(int xStep) {
@@ -65,11 +90,11 @@ class Balloon2 {
     }
 
     public void moveUp(int yStep) {
-        y += yStep;
+        y -= yStep;
     }
 
     public void moveDown(int yStep) {
-        y -= yStep;
+        y += yStep;
     }
 
     public void changeSize(int change) {
@@ -77,7 +102,7 @@ class Balloon2 {
     }
 
     public void display(Graphics paper) {
-        paper.setColor(Color.black);
+        paper.setColor(Color.red);
         paper.drawOval(x,y,diameter,diameter);
     }
 }
