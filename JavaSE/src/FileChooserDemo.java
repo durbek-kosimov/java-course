e368eff871beebc2b3117a375390e11b950b61e7
dmint <dkosimov@gmail.com>
2020-08-03 12:31:42 +0200

    File Chooser Demo

--- JavaSE/src/FileChooserDemo.java
+++ JavaSE/src/FileChooserDemo.java
@@ -2,5 +2,5 @@ import javax.swing.*;
 import java.awt.event.ActionListener;
 
 public class FileChooserDemo extends JFrame implements ActionListener {
-
+    private JButton openButton, saveButton;
 }
