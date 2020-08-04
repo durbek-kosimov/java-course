ff928a8d524db9730864493cb8c2fb03aafbf3b6
dmint <dkosimov@gmail.com>
2020-08-04 22:29:02 +0200

    File Chooser Demo

--- JavaSE/src/FileChooserDemo.java
+++ JavaSE/src/FileChooserDemo.java
@@ -5,4 +5,11 @@ public class FileChooserDemo extends JFrame implements ActionListener {
     private JButton openButton, saveButton;
     private JFileChooser fileChooser;
     private JTextField nameField;
+
+    public static void main(String[] args) {
+        FileChooserDemo frame = new FileChooserDemo();
+        frame.setSize(300, 150);
+        frame.createGUI();
+        frame.setVisible(true);
+    }
 }
