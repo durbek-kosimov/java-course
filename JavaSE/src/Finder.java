2212bfa8989e05eb591bf69113f4ecbe1c748414
dmint <dkosimov@gmail.com>
2020-08-08 14:55:26 +0200

    File Chooser Demo

---
+++ JavaSE/src/Finder.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+
+public class Finder {
+    private String line1, line2, line3;
+    private BufferedReader keyboard;
+    private BufferedReader inStream;
+
+    public static void main(String[] args) {
+        Finder aFind = new Finder();
+        aFind.doSearch();
+    }
+}
