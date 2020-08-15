b6dcd3d49327ef7187eb7540e518cd57000a4ff6
dmint <dkosimov@gmail.com>
2020-08-15 11:51:47 +0200

    Button Frame

--- JavaSE/src/ButtonFrame.java
+++ JavaSE/src/ButtonFrame.java
@@ -6,6 +6,14 @@ public class ButtonFrame extends JFrame {
     JButton unsubscribe = new JButton("Unsubscibe");
 
     public ButtonFrame() {
-
+        super("Button Frame");
+        setSize(340, 170);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        JPanel pane = new JPanel();
+        pane.add(load);
+        pane.add(save);
+        pane.add(unsubscribe);
+        add(pane);
+        setVisible(true);
     }
 }
