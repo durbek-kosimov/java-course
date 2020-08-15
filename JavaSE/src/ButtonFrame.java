826685404275bb5fd552fb51012efdeebc801a7c
dmint <dkosimov@gmail.com>
2020-08-15 11:48:50 +0200

    Button Frame

---
+++ JavaSE/src/ButtonFrame.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class ButtonFrame extends JFrame {
+    JButton load = new JButton("Load");
+    JButton save = new JButton("Save");
+    JButton unsubscribe = new JButton("Unsubscibe");
+
+    public ButtonFrame() {
+
+    }
+}
