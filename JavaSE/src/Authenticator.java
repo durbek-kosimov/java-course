fba1ddffa2955f9e406bc5e4dba2b5ea61e273ba
dmint <dkosimov@gmail.com>
2020-08-16 15:32:03 +0200

    Authenticator

---
+++ JavaSE/src/Authenticator.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Authenticator extends JFrame {
+    JTextField username = new JTextField();
+    JPasswordField password = new JPasswordField(15);
+    JTextArea comments = new JTextArea(4, 15);
+    JButton ok = new JButton("OK");
+    JButton cancel = new JButton("Cancel");
+
+
+}
