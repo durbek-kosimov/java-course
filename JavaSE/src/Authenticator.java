f4c6df2d0044b73ef2548d9882e36bba1b1cbf64
dmint <dkosimov@gmail.com>
2020-08-17 12:31:45 +0200

    Authenticator

--- JavaSE/src/Authenticator.java
+++ JavaSE/src/Authenticator.java
@@ -12,5 +12,21 @@ public class Authenticator extends JFrame {
         setSize(300, 220);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+        JPanel pane = new JPanel();
+        JLabel usernameLabel = new JLabel("Username: ");
+        JLabel passwordLabel = new JLabel("Password: ");
+        JLabel commentsLabel = new JLabel("Comments: ");
+        comments.setLineWrap(true);
+        comments.setWrapStyleWord(true);
+        pane.add(usernameLabel);
+        pane.add(username);
+        pane.add(passwordLabel);
+        pane.add(password);
+        pane.add(commentsLabel);
+        pane.add(comments);
+        pane.add(ok);
+        pane.add(cancel);
+        add(pane);
+        setVisible(true);
     }
 }
