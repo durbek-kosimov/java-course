69cb27f93d9b42c08b769b83a7ffcfa553fb1d40
dmint <dkosimov@gmail.com>
2020-08-17 14:03:58 +0200

    Format Frame 2

---
+++ JavaSE/src/FormatFrame2.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+public class FormatFrame2 extends JFrame {
+    String[] formats = {"Atom", "RSS 0.92", "RSS 1.0", "RSS 2.0"};
+    JComboBox formatBox = new JComboBox(formats);
+
+    public FormatFrame2() {
+        super("Choose a Format");
+        setSize(220, 150);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        JPanel pane = new JPanel();
+        JLabel formatLabel = new JLabel("Output formats");
+        pane.add(formatLabel);
+        pane.add(formatBox);
+        add(pane);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        FormatFrame2 ff = new FormatFrame2();
+    }
+}
