9ea7bd3dcbfd020a9450d56373cadd78c9a9797d
dmint <dkosimov@gmail.com>
2020-08-17 14:54:40 +0200

    Subscriptions

---
+++ JavaSE/src/Subscriptions.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+
+public class Subscriptions extends JFrame {
+    String[] subs = {"Burningbird", "Freedom Goodness", "Ideoplex", "Inessential", "Intertwingly",
+                     "Now This", "Rasterweb", "RC3", "Whole Lotta Nothing", "Workbench"};
+    JList<String> subList = new JList<>(subs);
+
+    public Subscriptions() {
+        super("Subscriptions");
+        setSize(180, 335);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        JPanel panel = new JPanel();
+        JLabel subLabel = new JLabel("RSS Subscription");
+        panel.add(subLabel);
+        subList.setVisibleRowCount(8);
+        JScrollPane scroller = new JScrollPane(subList);
+        panel.add(scroller);
+        add(panel);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        Subscriptions app = new Subscriptions();
+    }
+}
