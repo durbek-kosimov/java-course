7565e34e676cb7e8ad863c4eff0e434bf0024050
dmint <dkosimov@gmail.com>
2020-08-21 17:15:28 +0200

    Stacker

---
+++ src/Stacker.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Stacker extends JFrame {
+    public Stacker() {
+        super("Stacker");
+        setSize(430, 150);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+    }
+}
