e7c61993b3dd5b954ce2bd864406164e5c5d413f
dmint <dkosimov@gmail.com>
2020-08-22 20:27:22 +0200

    Stacker

--- src/Stacker.java
+++ src/Stacker.java
@@ -5,6 +5,15 @@ public class Stacker extends JFrame {
         super("Stacker");
         setSize(430, 150);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLookAndFeel();
+//        create top panel
+        JPanel commandPane = new JPanel();
+        BoxLayout horizontal = new BoxLayout(commandPane, BoxLayout.X_AXIS);
+        commandPane.setLayout(horizontal);
+        JButton subscribe = new JButton("Subscribe");
+        JButton unsubscribe = new JButton("Unsubscribe");
+        JButton refresh = new JButton("Refresh");
+        JButton save = new JButton("Save");
 
     }
 }
