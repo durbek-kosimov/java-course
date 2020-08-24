9bed29ac633d1d8e8b8d0cd4e30f2fac75339458
dmint <dkosimov@gmail.com>
2020-08-24 18:49:27 +0200

    SurveyWizard

---
+++ JavaSE/src/SueveyWizard.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class SurveyWizard extends JPanel implements ActionListener {
+    int currentCard = 0;
+    CardLayout cards = new CardLayout();
+    SurveyPanel[] ask = SurveyPanel[3];
+
+
+}
