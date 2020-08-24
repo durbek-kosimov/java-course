a1a900ba7843ea188a8f60a1f277aa061d3e97bb
dmint <dkosimov@gmail.com>
2020-08-24 19:15:34 +0200

    SurveyWizard

--- JavaSE/src/SueveyWizard.java
+++ JavaSE/src/SueveyWizard.java
@@ -12,5 +12,16 @@ public class SurveyWizard extends JPanel implements ActionListener {
         setSize(240, 140);
         setLayout(cards);
 //        set up servey
+        String question1 = "What is your gender?";
+        String[] resp1 = {"female", "male", "not telling"};
+        ask[0] = new SurveyPanel(question1, resp1, 2);
+        String question2 = "What is your age?";
+        String[] resp2 = {"Under 25", "25 - 34", "35 - 54", "Over 54"};
+        ask[1] = new SurveyPanel(question2, resp2, 1);
+        String question3 = "How often do you exercise each week?";
+        String[] resp3 = {"Never", "1 - 3 times", "More than 3"};
+        ask[2] = new SurveyPanel(question3, resp3, 1);
+        ask[2].setFinalQuestion(true);
+        addListeners();
     }
 }
