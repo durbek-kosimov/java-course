51650d6eee6896700fbb4ffa26713fc586212534
dmint <dkosimov@gmail.com>
2020-08-29 20:10:22 +0200

    Mouse prank

--- JavaSE/src/MousePrank.java
+++ JavaSE/src/MousePrank.java
@@ -1,7 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 
 public class MousePrank extends JFrame implements ActionListener {
     public MousePrank() {
@@ -41,4 +44,54 @@ public class MousePrank extends JFrame implements ActionListener {
     }
 }
 
+class PrankPanel extends JPanel implements MouseMotionListener {
+    JButton ok = new JButton("OK");
+    int buttonX, buttonY, mouseX, mouseY;
+    int width, height;
 
+    PrankPanel() {
+        super();
+        setLayout(null);
+        addMouseMotionListener(this);
+        buttonX = 110;
+        buttonY = 110;
+        ok.setBounds(new Rectangle(buttonX, buttonY, 70, 20));
+        add(ok);
+    }
+
+    public void mouseMoved(MouseEvent event) {
+        mouseX = event.getX();
+        mouseY = event.getY();
+        width = (int) getSize().getWidth();
+        height = (int) getSize().getHeight();
+        if (Math.abs((mouseX + 35) - buttonX) < 50) {
+            buttonX = moveButton(mouseX, buttonX, width);
+            repaint();
+        }
+        if (Math.abs((mouseY + 10) - buttonY) < 50) {
+            buttonY = moveButton(mouseY, buttonY, height);
+            repaint();
+        }
+    }
+
+    public void mouseDragged(MouseEvent event) {
+//        ignore this event
+    }
+
+    private int moveButton(int mouseAt, int buttonAt, int bord) {
+        if (buttonAt < mouseAt)
+            buttonAt --;
+        else
+            buttonAt ++;
+        if (buttonAt > (bord - 20))
+            buttonAt = 10;
+        if (buttonAt < 0)
+            buttonAt = bord - 80;
+        return buttonAt;
+    }
+
+    public void paintComponent(Graphics comp) {
+        super.paintComponent(comp);
+        ok.setBounds(buttonX, buttonY, 70, 20);
+    }
+}
