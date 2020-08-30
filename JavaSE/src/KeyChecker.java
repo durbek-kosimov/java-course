f980e52d2e7e8d95575f227b4b193c9ac1d78163
dmint <dkosimov@gmail.com>
2020-08-30 20:05:20 +0200

    Key Checker

--- JavaSE/src/KeyChecker.java
+++ JavaSE/src/KeyChecker.java
@@ -1,7 +1,20 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class KeyChecker extends JFrame {
     JLabel keyLabel = new JLabel("Hit any key");
+
+    public KeyChecker() {
+        super("Hit a Key");
+        setSize(300, 200);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new FlowLayout(FlowLayout.CENTER));
+        KeyMonitor monitor = new KeyMonitor(this);
+        setFocusable(true);
+        addKeyListener(monitor);
+        add(keyLabel);
+        setVisible(true);
+    }
 
 
 }
