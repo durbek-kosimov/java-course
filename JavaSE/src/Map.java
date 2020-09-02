835042a45354cf1721471d3dc48bf0da05466db2
dmint <dkosimov@gmail.com>
2020-09-02 23:34:31 +0200

    Map

--- JavaSE/src/Map.java
+++ JavaSE/src/Map.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Arc2D;
 import java.awt.geom.Rectangle2D;
 
 public class Map extends JFrame {
@@ -24,6 +25,18 @@ class MApPane extends JPanel {
         comp2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         Rectangle2D.Float background = new Rectangle2D.Float(0F, 0F, getSize().width, getSize().height);
         comp2d.fill(background);
+//        Draw waves
+        comp2d.setColor(Color.white);
+        BasicStroke pen = new BasicStroke(2F, BasicStroke.CAP_BUTT, BasicStroke.JOIN_ROUND);
+        comp2d.setStroke(pen);
+        for (int ax = 0; ax < 340; ax += 10) {
+            for (int ay = 0; ay < 340; ay += 10) {
+                Arc2D.Float wave = new Arc2D.Float(ax, ay, 10, 10, 0, -180, Arc2D.OPEN);
+                comp2d.draw(wave);
+            }
+        }
+//        Draw Florida
+
     }
 
 }
