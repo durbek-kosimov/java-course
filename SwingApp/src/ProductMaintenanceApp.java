f8ea193cff519954934add2f536bc6739c6fc698
dmint <dkosimov@gmail.com>
2020-09-11 14:06:22 +0200

    Product Maintenance App

---
+++ SwingApp/src/ProductMaintenanceApp.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class ProductMaintenanceApp {
+    public static void main(String[] args) {
+        JFrame frame = new ProductMaintenanceFrame();
+        frame.setVisible(true);
+    }
+}
+
+
