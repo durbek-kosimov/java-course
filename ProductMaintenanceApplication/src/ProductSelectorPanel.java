90ccfbe6c6c7e09da150e7a1f79ae473042472d4
dmint <dkosimov@gmail.com>
2020-09-24 20:47:49 +0200

    Product Selector Panel

--- ProductMaintenanceApplication/src/ProductSelectorPanel.java
+++ ProductMaintenanceApplication/src/ProductSelectorPanel.java
@@ -1,9 +1,18 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionListener;
 
 public class ProductSelectorPanel extends JPanel implements ActionListener {
     public JComboBox productComboBox;
     private JLabel productLabel;
     boolean filling  = false;
+
+    public ProductSelectorPanel() {
+//        set panel layout
+        setLayout(new FlowLayout(FlowLayout.LEFT));
+//        product label
+        productLabel = new JLabel("Select Product:");
+        add(productLabel);
+    }
 
 }
