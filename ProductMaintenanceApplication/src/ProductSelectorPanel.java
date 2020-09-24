b963d445db6a29ce832fcc3f4a2b9d36adf5b243
dmint <dkosimov@gmail.com>
2020-09-24 20:45:18 +0200

    Product Selector Panel

---
+++ ProductMaintenanceApplication/src/ProductSelectorPanel.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class ProductSelectorPanel extends JPanel implements ActionListener {
+    public JComboBox productComboBox;
+    private JLabel productLabel;
+    boolean filling  = false;
+
+}
