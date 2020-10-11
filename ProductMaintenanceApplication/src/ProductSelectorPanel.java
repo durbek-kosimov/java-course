d28aaca2c32c4a6f6dfce200092595450f25f900
dmint <dkosimov@gmail.com>
2020-10-11 15:46:14 +0200

    Product Selector Panel

--- ProductMaintenanceApplication/src/ProductSelectorPanel.java
+++ ProductMaintenanceApplication/src/ProductSelectorPanel.java
@@ -217,3 +217,15 @@ class EditListener implements ActionListener {
     }
 }
 
+class DeleteListener implements ActionListener {
+    public void actionPerformed(ActionEvent e) {
+        Product p = productPanel.getProduct();
+        productDAO.deleteProduct(p);
+        products.remove(p);
+        selectorPanel.fillComboBox(products);
+        selectorPanel.selectProduct(products.get(0));
+        productPanel.showProduct(products.get(0));
+        selectorPanel.productComboBox.requestFocusInWindow();
+    }
+}
+
