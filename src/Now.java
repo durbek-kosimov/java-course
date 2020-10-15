18f766d27e30d7598af6793e1a9bc1feb7d61462
dmint <dkosimov@gmail.com>
2020-10-15 11:38:57 +0200

    Examples of using Date class

---
+++ src/Now.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Now {
+    public static void main(String[] args) {
+        Date nu = new Date();
+        String s = nu.toString();
+        System.out.println(nu);
+        System.out.println(nu.toString());
+        System.out.println(s);
+    }
+}
