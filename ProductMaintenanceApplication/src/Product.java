b52bd2f1dec08b35dc738e4017980e7e3baf27bc
dmint <dkosimov@gmail.com>
2020-10-25 21:02:17 +0100

    Product Maintenance Application

---
+++ ProductMaintenanceApplication/src/Product.java
@@ -0,0 +1,47 @@
+import java.text.NumberFormat;
+
+public class Product implements Printable {
+    private String code;
+    private String description;
+    private double price;
+
+    public Product(String code, String description, double price) {
+        this.code = code;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public String getFormattedPrice() {
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        return currency.format(price);
+    }
+    @Override
+    public void print() {
+        System.out.println("Code:\t\t" + code);
+        System.out.println("Description:\t" + description);
+        System.out.println("price:\t\t" + this.getFormattedPrice());
+    }
+}
