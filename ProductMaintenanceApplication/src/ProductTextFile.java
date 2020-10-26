0057ee8a2c743b8d43145a851bd3d4d9a72c1200
dmint <dkosimov@gmail.com>
2020-10-26 11:24:49 +0100

    Product Maintenance Application

--- ProductMaintenanceApplication/src/ProductTextFile.java
+++ ProductMaintenanceApplication/src/ProductTextFile.java
@@ -1,2 +1,6 @@
-public class ProductTextFile {
+import java.io.File;
+
+public class ProductTextFile implements ProductDAO {
+    private File productsFile = null;
+    private final String FIELD_SEP = "\t";
 }
