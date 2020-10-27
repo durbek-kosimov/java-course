bbb0fd0766f5171842f6bdd1adb988dd098519f4
dmint <dkosimov@gmail.com>
2020-10-27 13:37:03 +0100

    Product Maintenance Application

--- ProductMaintenanceApplication/src/ProductTextFile.java
+++ ProductMaintenanceApplication/src/ProductTextFile.java
@@ -14,16 +14,46 @@ public class ProductTextFile implements ProductDAO {
             productsFile.createNewFile();
     }
 
-    private boolean saveProducts(ArrayList<Product> products) {
-        ProductWriter out = null;
+//    private boolean saveProducts(ArrayList<Product> products) {
+//        ProductWriter out = null;
+//        try {
+//            this.checkFile();
+//            out = new PrintWriter(new BufferedWriter(new FileWriter(productsFile)));
+//            for (Product p : products) {
+//                out.print(p.getCode() + FIELD_SEP);
+//                out.print(p.getDescription() + FIELD_SEP);
+//                out.print(p.getPrice());
+//            }
+//        }
+//        catch (IOException ioe) {
+//            ioe.printStackTrace();
+//            return false;
+//        }
+//        finally {
+//            this.close(out);
+//        }
+//        return true;
+//    }
+
+    public ArrayList<Product> getProducts() {
+        BufferedReader in = null;
         try {
             this.checkFile();
-            out = new PrintWriter(new BufferedWriter(new FileWriter(productsFile)));
-            for (Product p : products) {
-                out.print(p.getCode() + FIELD_SEP);
-                out.print(p.getDescription() + FIELD_SEP);
-                out.print(p.getPrice());
+            in = new BufferedReader(new FileReader(productsFile));
+            ArrayList<Product> products = new ArrayList<Product>();
+            String line  = in.readLine();
+            while (line != null) {
+                String[] columns = line.split(FIELD_SEP);
+                String code = columns[0];
+                String description = columns[1];
+                String price = columns[2];
+
+                Product p = new Product(code, description, Double.parseDouble(price));
+                products.add(p);
+                line = in.readLine();
             }
+            return products;
         }
+
     }
 }
