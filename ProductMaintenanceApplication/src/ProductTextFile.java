12f76147a6b655c75eabcc29f19cac48bdf2bda0
dmint <dkosimov@gmail.com>
2020-10-28 20:48:38 +0100

    Product Maintenance Application

--- ProductMaintenanceApplication/src/ProductTextFile.java
+++ ProductMaintenanceApplication/src/ProductTextFile.java
@@ -1,112 +1,114 @@
-//package murach.db;
-
 import java.io.*;
 import java.nio.file.*;
 import java.util.*;
+public class ProductTextFile implements ProductDAO {
 
-//import murach.business.Product;
-//public final class ProductTextFile implements DAO<Product> {
-public final class ProductTextFile implements DAO<Product> {
-
-    private List<Product> products = null;
-    private Path productsPath = null;
     private File productsFile = null;
     private final String FIELD_SEP = "\t";
 
     public ProductTextFile() {
-        productsPath = Paths.get("products.txt");
-        productsFile = productsPath.toFile();
-        products = this.getAll();
+        productsFile = new File("products.txt");
     }
 
-    @Override
-    public List<Product> getAll() {
-        // if the products file has already been read, don't read it again
-        if (products != null) {
-            return products;
-        }
-
-        products = new ArrayList<>();
-        if (Files.exists(productsPath)) {
-            try (BufferedReader in = new BufferedReader(
-                    new FileReader(productsFile))) {
-
-                // read products from file into array list
-                String line = in.readLine();
-                while (line != null) {
-                    String[] fields = line.split(FIELD_SEP);
-                    String code = fields[0];
-                    String description = fields[1];
-                    String price = fields[2];
-
-                    Product p = new Product(
-                            code, description, Double.parseDouble(price));
-                    products.add(p);
-
-                    line = in.readLine();
-                }
-            } catch (IOException e) {
-                // System.out.println(e);
-                return null;
-            }
-        } else {
-            // System.out.println(productsPath.toAbsolutePath() + " doesn't exist.");
-            return null;
-        }
-        return products;
+    private void checkFile() throws IOException {
+//        if the file doesn't exist, create it
+        if (!productsFile.exists())
+            productsFile.createNewFile();
     }
 
-    @Override
-    public Product get(String code) {
-        for (Product p : products) {
-            if (p.getCode().equals(code)) {
-                return p;
-            }
-        }
-        return null;
-    }
-
-    private boolean saveAll() {
-        try (PrintWriter out = new PrintWriter(
-                new BufferedWriter(
-                        new FileWriter(productsFile)))) {
-
-            // write all products in the array list
-            // to the file
+    private boolean saveProducts(ArrayList<Product> products) {
+        PrintWriter out = null;
+        try {
+            this.checkFile();
+//            open output stream for overwriting
+            out = new PrintWriter(new BufferedWriter(new FileWriter(productsFile)));
+//            write all products in the array list to the file
             for (Product p : products) {
                 out.print(p.getCode() + FIELD_SEP);
                 out.print(p.getDescription() + FIELD_SEP);
                 out.println(p.getPrice());
             }
-            return true;
-        } catch (IOException e) {
-            System.out.println(e);
+        }
+        catch (IOException ioe) {
+            ioe.printStackTrace();
             return false;
+        }
+        finally {
+            this.close(out);
+        }
+        return true;
+    }
+
+    private void close(Closeable stream) {
+        try {
+            if (stream != null)
+                stream.close();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    public ArrayList<Product> getProducts() {
+        BufferedReader in = null;
+        try {
+            this.checkFile();
+            in = new BufferedReader(new FileReader(productsFile));
+            ArrayList<Product> products = new ArrayList<Product>();
+//            read all products stored in the file into the array list
+            String line = in.readLine();
+            while (line != null) {
+                String[] columns = line.split(FIELD_SEP);
+                String code = columns[0];
+                String description = columns[1];
+                String price = columns[2];
+                Product p = new Product(code, description, Double.parseDouble(price));
+                products.add(p);
+                line = in.readLine();
+            }
+            return products;
+        }
+        catch (IOException ioe) {
+            ioe.printStackTrace();
+            return null;
+        }
+        finally {
+            this.close(in);
         }
     }
 
     @Override
-    public boolean add(Product p) {
+    public Product getProduct(String code) {
+        ArrayList<Product> products = this.getProducts();
+        for (Product p : products) {
+            if (p.getCode().equals(code))
+                return p;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean addProduct(Product p) {
+        ArrayList<Product> products = this.getProducts();
         products.add(p);
-        return this.saveAll();
+        return this.saveProducts(products);
     }
 
     @Override
-    public boolean delete(Product p) {
+    public boolean deleteProduct(Product p) {
+        ArrayList<Product> products = this.getProducts();
         products.remove(p);
-        return this.saveAll();
+        return this.saveProducts(products);
     }
 
     @Override
-    public boolean update(Product newProduct) {
-        // get the old product and remove it
-        Product oldProduct = this.get(newProduct.getCode());
+    public boolean updateProduct(Product newProduct) {
+        ArrayList<Product> products = this.getProducts();
+//        get the old product and remove it
+        Product oldProduct = this.getProduct(newProduct.getCode());
         int i = products.indexOf(oldProduct);
         products.remove(i);
-
-        // add the updated product
+//        add the updated product
         products.add(i, newProduct);
-
-        return this.saveAll();
+        return this.saveProducts(products);
     }
 }
