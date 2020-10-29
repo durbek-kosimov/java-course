d7a75d4d52f77dc0ec0b608846b5657c26916431
dmint <dkosimov@gmail.com>
2020-10-29 20:29:27 +0100

    Product Maintenance Application

--- ProductMaintenanceApplication/src/ProductMaintenancePanel.java
+++ ProductMaintenanceApplication/src/ProductMaintenancePanel.java
@@ -229,5 +229,62 @@ class ProductMaintenancePanel extends JPanel {
         }
     }
 
+    class EditListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            buttonPanel.setAddEditMode(true);
+            productPanel.setEditMode();
+        }
+    }
 
+    class DeleteListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            Product p = productPanel.getProduct();
+            productDAO.deleteProduct(p);
+            products.remove(p);
+            selectorPanel.fillComboBox(products);
+            selectorPanel.selectProduct(products.get(0));
+            productPanel.showProduct(products.get(0));
+            selectorPanel.productComboBox.requestFocusInWindow();
+        }
+    }
+
+    class AcceptListener implements ActionListener {
+        public void actionPerfermed(ActionEvent e) {
+            if (isValidData()) {
+                if (newProduct != null) {
+                    newProduct = productPanel.getProduct();
+                    productDAO.addProduct(newProduct);
+                    products.add(newProduct);
+                    selectorPanel.fillComboBox(products);
+                    selectorPanel.selectProduct(newProduct);
+                    newProduct = null;
+                }
+                else {
+                    Product p = selectorPanel.getCurrentProduct();
+                    Product newProduct = productPanel.getProduct();
+                    p.setDescription(newProduct.getDescription());
+                    p.setPrice(newProduct.getPrice());
+                    productDAO.updateProduct(p);
+                    selectorPanel.fillComboBox(products);
+                    selectorPanel.selectProduct(p);
+                    productPanel.showProduct(selectorPanel.getCurrentProduct());
+                }
+                productPanel.setDisplayMode();
+                buttonPanel.setAddEditMode(false);
+                selectorPanel.productComboBox.requestFocusInWindow();
+            }
+        }
+
+        public boolean isValidData() {
+            if (newProduct != null)
+                return SwingValidator.isPresent(productPanel.codeTextField, "Product Code")
+                    && SwingValidator.isPresent(productPanel.descriptionTextFiled, "Description")
+                    && SwingValidator.isPresent(productPanel.priceTextField, "Unit Price")
+                    && SwingValidator.isDouble(productPanel.priceTextField, "Unit Price");
+            else
+                return SwingValidator.isPresent(productPanel.descriptionTextFiled, "Description")
+                    && SwingValidator.isPresent(productPanel.priceTextField, "Unit Price")
+                    && SwingValidator.isDouble(productPanel.priceTextField, "Unit Price");
+        }
+    }
 }
