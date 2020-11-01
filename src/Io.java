7423695786b809d4071e2811809b24e24bdf1516
dmint <dkosimov@gmail.com>
2020-11-01 21:55:41 +0100

    File IO and Making Directory

--- src/Io.java
+++ src/Io.java
@@ -1,10 +1,18 @@
 import java.io.File;
+import java.io.IOException;
 
 public class Io {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String dirName = "/home/dmint/Desktop/io";
+        String fileName = "products.txt";
         File dir = new File(dirName);
-        if (!dir.exists())
+        File productsFile = new File(dirName + fileName);
+        if (!dir.exists()) {
             dir.mkdir();
+        }
+
+//        System.out.println(dirName + fileName);
+        if (!productsFile.exists())
+            productsFile.createNewFile();
     }
 }
