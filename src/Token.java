f46f2dd387eb8bb1b4cd3e3e8bec1dbb6e00d66d
dmint <dkosimov@gmail.com>
2020-11-04 12:37:01 +0100

    Working with command line arguments

---
+++ src/Token.java
@@ -0,0 +1,10 @@
+import java.util.StringTokenizer;
+
+public class Token {
+    public static void main(String[] args) {
+        String str = "initUcell 20 20 20";
+        str = str.trim();
+        StringTokenizer st = new StringTokenizer(str, " ");
+        System.out.println("Spacies are: " + st.countTokens());
+    }
+}
