60f2df0091d8a28c87cca150a1255ce00396d622
dmint <dkosimov@gmail.com>
2020-11-05 11:52:28 +0100

    Input/Output example

--- src/Io.java
+++ src/Io.java
@@ -5,22 +5,31 @@ import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Io {
-
     public static void main(String[] args) throws IOException {
         File pr = new File("/home/dmint/csi/pr3/pr_03_1.in");
         BufferedReader in = new BufferedReader(new FileReader(pr));
+        Double deltaT, density, temperature;
+        Integer stepAvg, stepEquil, stepInitlzTemp, stepLimit;
+
         String line;
         String value = null;
         String description = null;
         String x = null, y = null, z = null;
         int indexOfSpace;
         int count = 0;
+        int countLine = 0;
         while ((line = in.readLine()) != null) {
             line = line.trim();
+            StringTokenizer stLine = new StringTokenizer(line, " ");
+            countLine = stLine.countTokens();
             indexOfSpace = line.indexOf(" ");
             String[] str = line.split(" +");
             description = line.substring(0, indexOfSpace);
             value = line.substring(indexOfSpace + 1);
+            if ((countLine == 1 || countLine > 2) && !description.equals("initUcell")) {
+                System.out.println("Incompatible data!!!");
+                break;
+            }
             StringTokenizer st = new StringTokenizer(value, " ");
             count = st.countTokens();
             if (description.equals("initUcell") && count > 1) {
@@ -31,7 +40,7 @@ public class Io {
                     }
                     System.out.print("initUcell = " + x + " " + y);
                     System.out.println("\tEvery thing is OK! Token = 2");
-                    continue;
+//                    continue;
                 }
                 else if (count == 3) {
                     while (st.hasMoreTokens()) {
@@ -41,16 +50,13 @@ public class Io {
                     }
                     System.out.print("initUcell = " + x + " " + y + " " + z);
                     System.out.println("\tEvery thing is OK! Token = 3");
-                    continue;
+//                    continue;
                 }
                 else
                     System.out.println("Too many data!");
-
             } else
                 System.out.println(description + " " + value);
-            continue;
         }
         in.close();
     }
 }
-
---
+++ src/Io2.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Io2 {
+    public static void main(String[] args) throws IOException {
+        File pr = new File("/home/dmint/csi/pr3/pr_03_1.in");
+        BufferedReader in = new BufferedReader(new FileReader(pr));
+        Double deltaT, density, temperature;
+        Integer stepAvg, stepEquil, stepInitlzTemp, stepLimit;
+
+        String line;
+        String value = null;
+        String description = null;
+        String x = null, y = null, z = null;
+        int countLine = 1;
+        int countToken = 0;
+        while ((line = in.readLine()) != null) {
+            line = line.trim();
+            StringTokenizer stLine = new StringTokenizer(line, " ");
+            countToken = stLine.countTokens();
+            switch (countToken) {
+                case 1:
+                    System.out.println("Incopatible data!!! In line " + countLine);
+                    break;
+                case 2:
+                    description = stLine.nextToken();
+                    if (description.equals("initUcell"))
+                        System.out.println("Few data! In line " + countLine);
+                    while (stLine.hasMoreTokens()) {
+                        value = stLine.nextToken();
+                    }
+                    break;
+                case 3:
+                    description = stLine.nextToken();
+                    if (description.equals("initUcell")) {
+                        while (stLine.hasMoreTokens()) {
+                            x = stLine.nextToken();
+                            y = stLine.nextToken();
+                        }
+
+                    }
+                    else
+                        System.out.println("Too many data! In line " + countLine);
+                        break;
+                case 4:
+                    description = stLine.nextToken();
+                    if (description.equals("initUcell")) {
+                        while (stLine.hasMoreTokens()) {
+                            x = stLine.nextToken();
+                            y = stLine.nextToken();
+                            z = stLine.nextToken();
+                        }
+                    }
+                    else
+                        System.out.println("Too many data! In line " + countLine);
+                    break;
+                case 5:
+                    System.out.println("Too many data!!! In line " + countLine);
+                    break;
+            }
+            if (countToken == 2)
+                System.out.println(description + " " + value);
+            else if (countToken == 3)
+                System.out.println(description + " " + x + " " + y);
+            else if (countToken == 4)
+                System.out.println(description + " " + x + " " + y + " " + z);
+            countLine ++;
+        }
+        in.close();
+    }
+}
