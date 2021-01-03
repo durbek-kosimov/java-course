3de3bbcd4809b98de027afe0893fc82aeff407de
dkmint <dkosimov@gmail.com>
2021-01-03 20:52:58 +0100

    MicroGolf Project

--- MicroGolf/src/Stove.java
+++ MicroGolf/src/Stove.java
@@ -6,6 +6,7 @@ public class Stove {
     private lamp currentLamp;
     private door currentDoor;
     private power currentPower;
+    private int time;
 
     public Stove(lamp currentLamp, door currentDoor, power currentPower) {
         this.currentLamp = lamp.on;
@@ -16,6 +17,10 @@ public class Stove {
     private void takeButton() {
         if (currentDoor == door.open)
             System.out.println("Pressing the button when the door is open is meaningless.");
+        else if (currentPower == power.on) {
+            time += 60;
+            System.out.println("One minute is added.");
+        }
 
     }
 }
