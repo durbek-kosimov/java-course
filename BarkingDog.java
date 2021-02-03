b8d89780a0f9c4c146b9c72d788e56a8737bd896
durbek kosimov <26488170+durbek-kosimov@users.noreply.github.com>
2021-02-03 23:13:51 +0100

    BarkingDog Class Returns Boolean Type

---
+++ BarkingDog.java
@@ -0,0 +1,12 @@
+package com;
+
+public class BarkingDog {
+    public static boolean shouldWakeUp(boolean isBarking, int hourOfDay) {
+        if (hourOfDay < 0 || hourOfDay > 23)
+            return false;
+        else if (isBarking && (hourOfDay < 8 || hourOfDay > 22))
+                return true;
+        else
+            return false;
+    }
+}
